2d81845750676282da3728d2ba9b069bfd011bba
DESKTOP-18P8HD2\user <sh1222j@gmail.com>
2022-07-26 17:26:28 +0900

    시작

---
+++ 001 004 print 기초.py
@@ -0,0 +1,11 @@
+# 001 print 기초
+print("Hello World")
+
+# 002 print 기초
+print("Mary's cosmetics")
+
+# 003 print 기초
+print('신씨가 소리질렀다. "도둑이야!"')
+
+# 004 print 기초
+print('C:\windows')
---
+++ practice.py
@@ -0,0 +1,29 @@
+# 2-1 숫자 자료형
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+
+# 문자열 자료형
+print('풍선')
+print('나비')
+print('ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ')
+print('ㅋ'*3)
+
+# boolean 자료형
+# 참 / 거짓
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
+
+
+
+
+
