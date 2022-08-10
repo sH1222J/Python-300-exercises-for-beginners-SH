188b90687860abb15ce27210a0b63f431823f3cb
DESKTOP-18P8HD2\user <sh1222j@gmail.com>
2022-08-10 13:06:19 +0900

    출처 표기 / 6번까지 풀음

--- 001 004 print 기초.py
+++ 001 004 print 기초.py
@@ -1,3 +1,5 @@
+# https://wikidocs.net/7014
+
 # 001 print 기초
 print("Hello World")
 
@@ -14,3 +16,10 @@ print('C:\windows')
 # 다음 코드를 실행해보고 \t와 \n의 역할을 설명해보세요.
 # \t는 탭, \n줄바꿈이다.
 print("안녕하세요.\n 만나서 \t\t 반갑습니다.")
+
+# 006 print 여러 데이터 출력
+# print("오늘은", "일요일")
+# 여러 값을 출력시 print 함수에 쉼표로 구분주면됨
+
+# 007 print 기초
+print()
